ee4c9dc1f697977bb5d58ce9b455e9dbc9e3f455
William Alves <williamalvesroot@gmail.com>
2020-10-10 22:25:12 -0300

    Adicionando configurações do DRF

--- message_scheduler/settings.py
+++ message_scheduler/settings.py
@@ -56,6 +56,14 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
+}
+
 ROOT_URLCONF = 'message_scheduler.urls'
 
 TEMPLATES = [
